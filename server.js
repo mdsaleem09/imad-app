911d4a0d5a7cfd0cba0d0634ec395aa3508aa47d
mdsaleem09 <30777025+mdsaleem09@users.noreply.github.com>
2017-08-12 16:25:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res){
-     res.send(createTemplate(articleone));
+     res.send(createTemplate(article-one));
 });
 app.get('/article-three', function (req, res){
      res.sendFile(path.join(__dirname, 'article-three.html'));
